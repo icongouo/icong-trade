135d275969361c3f0135af62318efdab718a057c
icongouo <1261318485@qq.com>
2025-04-30 00:00:00 +0800

    commit23:tcc开发

--- trade-coupon/src/main/java/com/shangan/trade/coupon/db/dao/CouponDao.java
+++ trade-coupon/src/main/java/com/shangan/trade/coupon/db/dao/CouponDao.java
@@ -5,17 +5,36 @@ import com.shangan.trade.coupon.db.model.Coupon;
 import java.util.List;
 
 public interface CouponDao {
+
     /**
      * 新增一张券
+     *
      * @param coupon
      * @return
      */
     boolean insertCoupon(Coupon coupon);
 
     /**
-     *  查询用的优惠券
+     * 查询用的优惠券
+     *
      * @param userId
      * @return
      */
     List<Coupon> queryUserCoupons(long userId);
+
+
+    /**
+     * 通过券的id查询对应的券
+     *
+     * @param couponId
+     * @return
+     */
+    Coupon queryCouponById(long couponId);
+
+    /**
+     * 更新券的信息
+     * @param coupon
+     * @return
+     */
+    boolean updateCoupon(Coupon coupon);
 }
--- trade-coupon/src/main/java/com/shangan/trade/coupon/db/dao/impl/CouponDaoImpl.java
+++ trade-coupon/src/main/java/com/shangan/trade/coupon/db/dao/impl/CouponDaoImpl.java
@@ -3,18 +3,14 @@ package com.shangan.trade.coupon.db.dao.impl;
 import com.shangan.trade.coupon.db.dao.CouponDao;
 import com.shangan.trade.coupon.db.mappers.CouponMapper;
 import com.shangan.trade.coupon.db.model.Coupon;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-/**
- * 券-数据库操作
- */
-@Slf4j
 @Service
 public class CouponDaoImpl implements CouponDao {
+
     @Autowired
     private CouponMapper couponMapper;
 
@@ -28,5 +24,15 @@ public class CouponDaoImpl implements CouponDao {
     @Override
     public List<Coupon> queryUserCoupons(long userId) {
         return couponMapper.queryUserCoupons(userId);
+    }
+
+    @Override
+    public Coupon queryCouponById(long couponId) {
+        return couponMapper.selectByPrimaryKey(couponId);
+    }
+
+    @Override
+    public boolean updateCoupon(Coupon coupon) {
+        return couponMapper.updateByPrimaryKey(coupon) > 0;
     }
 }
