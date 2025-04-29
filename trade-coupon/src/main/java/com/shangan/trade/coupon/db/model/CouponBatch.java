c2a314fe06b0a5744c4709796b5cccf66e05d725
icongouo <1261318485@qq.com>
2025-04-30 00:00:00 +0800

    update:剩余总量添加字段后修改

--- trade-coupon/src/main/java/com/shangan/trade/coupon/db/model/CouponBatch.java
+++ trade-coupon/src/main/java/com/shangan/trade/coupon/db/model/CouponBatch.java
@@ -15,6 +15,8 @@ public class CouponBatch {
 
     private Long totalCount;
 
+    private Long availableCount;
+
     private Long assignCount;
 
     private Long usedCount;
@@ -71,6 +73,14 @@ public class CouponBatch {
 
     public void setTotalCount(Long totalCount) {
         this.totalCount = totalCount;
+    }
+
+    public Long getAvailableCount() {
+        return availableCount;
+    }
+
+    public void setAvailableCount(Long availableCount) {
+        this.availableCount = availableCount;
     }
 
     public Long getAssignCount() {
