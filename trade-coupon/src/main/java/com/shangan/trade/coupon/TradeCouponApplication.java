151264f28e9190c97faa8890fbedba6cd1a8bb64
icongouo <1261318485@qq.com>
2025-04-30 00:00:00 +0800

    commit1:项目基础结构构建

---
+++ trade-coupon/src/main/java/com/shangan/trade/coupon/TradeCouponApplication.java
@@ -0,0 +1,13 @@
+package com.shangan.trade.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeCouponApplication.class, args);
+    }
+
+}
